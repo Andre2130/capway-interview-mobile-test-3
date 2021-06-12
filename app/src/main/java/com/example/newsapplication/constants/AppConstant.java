4055c8e497c5e2d3820142c760a314412e611e6e
andre2130 <atrobinson30@gmail.com>
2021-06-12 00:25:40 -0400

    Added api key for newsapi.org to AppConstant file

--- app/src/main/java/com/example/newsapplication/constants/AppConstant.java
+++ app/src/main/java/com/example/newsapplication/constants/AppConstant.java
@@ -1,4 +1,7 @@
 package com.example.newsapplication.constants;
 
 public class AppConstant {
+    public static final String BASE_URL="https://newsapi.org/v2/";
+
+    public static final String API_KEY="5e56f9160e2b4c809c677fce234cc03e";
 }
