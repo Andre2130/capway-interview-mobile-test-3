e5ec655996af1cdf6cb4b4867f4d242cb1b0827e
andre2130 <atrobinson30@gmail.com>
2021-06-12 00:40:41 -0400

    created retrofit interface to send http request to news api

--- app/src/main/java/com/example/newsapplication/retrofit/ApiRequest.java
+++ app/src/main/java/com/example/newsapplication/retrofit/ApiRequest.java
@@ -1,4 +1,17 @@
 package com.example.newsapplication.retrofit;
 
+import com.example.newsapplication.response.ArticleResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import static com.example.newsapplication.constants.AppConstant.API_KEY;
+
+
 public interface ApiRequest {
+
+    @GET("top-headlines?country=us&apiKey="+API_KEY)
+    Call<ArticleResponse> getTopHeadlines();
+
+
 }
