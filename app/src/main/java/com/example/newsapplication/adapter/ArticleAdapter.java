55e5766f0b65f8135505d567fc1ea20e432038e2
andre2130 <atrobinson30@gmail.com>
2021-06-12 01:11:59 -0400

    ArticleAdapter binds the Articles returned from the http request to the recyclerView

--- app/src/main/java/com/example/newsapplication/adapter/ArticleAdapter.java
+++ app/src/main/java/com/example/newsapplication/adapter/ArticleAdapter.java
@@ -1,4 +1,75 @@
 package com.example.newsapplication.adapter;
 
-public class ArticleAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.newsapplication.R;
+import com.example.newsapplication.model.Article;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHolder> {
+
+    private final Context context;
+    ArrayList<Article> articleArrayList;
+
+
+    public ArticleAdapter(Context context, ArrayList<Article> articleArrayList) {
+        this.context = context;
+        this.articleArrayList = articleArrayList;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_item,parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+
+        Article article = articleArrayList.get(position);
+        holder.tvTitle.setText(article.getTitle());
+        holder.tvAuthor.setText(article.getAuthor());
+        holder.tvDate.setText(article.getPublishedAt());
+        Glide.with(context)
+                .load(article.getUrlToImage())
+                .into(holder.imageViewIcon);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return articleArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imageViewIcon;
+        private final TextView tvTitle;
+        private final TextView tvAuthor;
+        private final TextView tvDate;
+
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            imageViewIcon = itemView.findViewById(R.id.imageViewIcon);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvAuthor = itemView.findViewById(R.id.tvAuthor);
+            tvDate = itemView.findViewById(R.id.tvDate);
+        }
+    }
 }
