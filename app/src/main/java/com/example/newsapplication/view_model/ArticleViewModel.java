7c851b446ad2bd5a9e91e59cd16a3efb10d01f5f
andre2130 <atrobinson30@gmail.com>
2021-06-12 01:21:39 -0400

    ArticleViewModel handels UI data and allows it to persist though configuration changes

--- app/src/main/java/com/example/newsapplication/view_model/ArticleViewModel.java
+++ app/src/main/java/com/example/newsapplication/view_model/ArticleViewModel.java
@@ -1,4 +1,30 @@
 package com.example.newsapplication.view_model;
 
-public class ArticleViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.newsapplication.repository.ArticleRepository;
+import com.example.newsapplication.response.ArticleResponse;
+
+public class ArticleViewModel extends AndroidViewModel {
+
+    private ArticleRepository articleRepository;
+    private LiveData<ArticleResponse> articleResponseLiveData;
+
+    public ArticleViewModel(@NonNull Application application) {
+
+
+        super(application);
+
+
+        articleRepository = new ArticleRepository();
+        this.articleResponseLiveData = articleRepository.getDashBoardNews();
+    }
+
+    public LiveData<ArticleResponse> getArticleResponseLiveData() {
+        return articleResponseLiveData;
+    }
 }
